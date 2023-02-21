73dae14f738d4dd3c3c53841f271ac0653538c84
Gautam Dheer <gautam.litesh@gnail.com>
2023-02-21 11:45:38 +0530

    save form data added

--- form.php
+++ form.php
@@ -10,39 +10,58 @@ $result = $obj->getResult();
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form </title>
     <style>
-        .container{max-width:1000px;margin:auto;}
-        .row{display:flex; justify-content: center;max-width:100%;}
-        form{width:100%;}
-        input, select{display:block;margin-bottom: 40px;line-height: 1.5;}
-        
+    .container {
+        max-width: 1000px;
+        margin: auto;
+    }
+
+    .row {
+        display: flex;
+        justify-content: center;
+        max-width: 100%;
+    }
+
+    form {
+        width: 100%;
+    }
+
+    input,
+    select {
+        display: block;
+        margin-bottom: 40px;
+        line-height: 1.5;
+    }
     </style>
 </head>
+
 <body>
     <div class="container">
         <div class="row">
-        <form action="save.php" method="post">
-            <label for="name">Name</label>
-            <input type="text" name="name">
-            
-            <label for="age">Age</label>
-            <input type="text" name="age">
-            
-            <label for="name">City</label>
-            <select name="" id="">
-                <?php foreach($result as list('id'=>$id,'city'=>$city)){ ?>
-                 <option value="<?php echo $id; ?>"><?php echo $city; ?></option>
-                 <?php } ?>
-            </select>
-            <input type="submit" value="Save">    
+            <form action="save.php" method="post">
+                <label for="name">Name</label>
+                <input type="text" name="name">
 
-        </form>
+                <label for="age">Age</label>
+                <input type="text" name="age">
+
+                <label for="name">City</label>
+                <select name="city" id="city">
+                    <?php foreach($result as list('id'=>$id,'city'=>$city)){ ?>
+                    <option value="<?php echo $city; ?>"><?php echo $city; ?></option>
+                    <?php } ?>
+                </select>
+                <input type="submit" name="submit" value="Save">
+
+            </form>
         </div>
     </div>
 </body>
+
 </html>
---
+++ save.php
@@ -0,0 +1,29 @@
+<?php
+include 'Database.php';
+$obj = new Database();
+
+echo "Insert id:-";
+print_r($obj->getResult());
+
+if(isset($_POST['submit'])){
+   
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+    $city = $_POST['city'];
+    
+    if(!empty($name) || !empty(age) || !empty($city)){
+        $obj->insert('students',['student_name'=>$name,'age'=>$age,'city'=>$city]);
+        echo "Data Inserted";
+    }
+    else{
+        echo "Please fill the required fields";
+    }
+
+
+}
+else{
+
+}
+
+
+?>
